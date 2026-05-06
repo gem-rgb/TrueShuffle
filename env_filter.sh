f2e0c716f5d4eec773f8f73b1a54116385bdbd19
Karani <karanitaitumu@gmail.com>
2026-05-06 04:28:52 +0300

    Update config and add utility scripts

---
+++ env_filter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export GIT_AUTHOR_EMAIL="tatetricky@gmail.com"
+export GIT_COMMITTER_EMAIL="tatetricky@gmail.com"
+export GIT_AUTHOR_NAME="Karani Taitumu"
+export GIT_COMMITTER_NAME="Karani Taitumu"
